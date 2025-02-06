d6a30327292794c0c5cedbea7f55c4617cd7b5d8
Nguyen Xuan Quang <quang456a@gmail.com>
2025-02-06 21:47:52 +0700

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        navbar
         {children}
+        footer
       </body>
     </html>
   );
